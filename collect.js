55df8d66aea78877433837213e6008c8aae65bc1
yanagiragi <qaz90473@gmail.com>
2018-02-26 03:16:46 +0800

    update new json

--- collect.js
+++ collect.js
@@ -32,10 +32,54 @@ function fromDir(startPath, filter, callback){
 
         resolve(container);
     })
-};
+}
 
-fromDir('D:\\_g2GoogleDrive\\_Source\\_Models\\Pmx','.pmd')
-    .then(fromDir('D:\\_g2GoogleDrive\\_Source\\_Models\\Pmx','.pmx'))
+function CheckfromDir(startPath, filter, callback){
+    return new Promise((resolve, reject) => {
+        
+        if (!fs.existsSync(startPath)){
+            console.log("no dir ",startPath);
+            resolve(container);
+        }
+
+        var files=fs.readdirSync(startPath);
+        for(var i=0;i<files.length;i++){
+            var filename=path.join(startPath,files[i]);
+            var stat = fs.lstatSync(filename);
+            if (stat.isDirectory()){
+                CheckfromDir(filename,filter); //recurse
+            }
+            else if (filename.indexOf(filter)>=0) {
+                var previewPath = filename.substring(0, filename.length - ".pmx".length) + "_preview.jpg"
+                if(filename.indexOf(".pm") == (filename.length - ".pmx".length)){
+                    // console.log(filename)
+                }
+                if(!fs.existsSync(previewPath) && filename.indexOf(".pm") == (filename.length - ".pmx".length)){
+                    name = (filename.lastIndexOf("\\") != -1) ? filename.substring(filename.lastIndexOf("\\") + 1, filename.length - ".pmx".length) : filename
+                    container.push({
+                        "name" : name,
+                        "path" : filename
+                    })
+                }
+            };
+        };
+
+        resolve(container);
+    })
+}
+
+var MMDpath = 'E:\\_Source\\_Models\\Pmx\\_pmx'
+
+
+fromDir(MMDpath,'.pmd')
+    .then(fromDir(MMDpath,'.pmx'))
     .then(()=>{
         console.log(JSON.stringify(container, null, 4))
     });
+
+
+/*CheckfromDir(MMDpath,'.pmd')
+    .then(CheckfromDir(MMDpath,'.pmx'))
+    .then(()=>{
+        console.log(JSON.stringify(container, null, 4))
+    });*/
